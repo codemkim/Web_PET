cc23016623eeda003148b9e7e06cc9dbd7f2c25c
hyeonukim <80312713+codemkim@users.noreply.github.com>
2021-10-31 14:06:57 +0900

    2021-10-31 loading edit 1

--- FirstService/models.py
+++ FirstService/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Profile(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='')
 
     def __str__(self):
         return self.title
