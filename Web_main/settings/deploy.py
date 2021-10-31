6d7e9880e576f5660569eb6fcabf343799fb04c7
hyeonukim <80312713+codemkim@users.noreply.github.com>
2021-10-31 15:54:29 +0900

    2021-10-31 DEPLOY edit 3

--- Web_main/settings/deploy.py
+++ Web_main/settings/deploy.py
@@ -1,12 +1,12 @@
 from .base import *
 
-def read_secreat(secreat_name):
-    file= open('/run/secreats/'+secreat_name )
-    secreat=file.read()
-    secreat=secreat.rstrip().lstrip()
+def read_secret(secret_name):
+    file= open('/run/secrets/'+secret_name)
+    secret=file.read()
+    secret=secret.rstrip().lstrip()
     file.close()
 
-    return secreat
+    return secret
 
 env = environ.Env(
     # set casting, default value
@@ -21,7 +21,7 @@ DEBUG = False
 
 # Raises Django's ImproperlyConfigured
 # exception if SECRET_KEY not in os.environ
-SECRET_KEY = read_secreat('DJANGO_SECREAT_KEY')
+SECRET_KEY = read_secret('DJANGO_SECREAT_KEY')
 
 ALLOWED_HOSTS = ['*']
 
@@ -33,7 +33,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': read_secreat('MYSQL_PASSWORD'),
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
